6aca3b34739b9709a42c88d1cafbae35d69fc657
Abhi-smhi <abhilash.murlidharanmenon@smhi.se>
2026-01-30 13:53:15 +0000

    Chunking files to reduce overheads

--- ddh2nc.py
+++ ddh2nc.py
@@ -30,14 +30,11 @@ def parse_ddh_attributes(file_path):
                 continue
     return attributes
 
-def worker_ddh(fasta_path, articles = None):
-    """
-    Worker: Runs Unix tools ddhr and lfac on DDH file fasta_path, extracts
-    articles, and returns a single xarray Dataset.
-    """
 
-#   print(f"PID [{os.getpid()}]: file {fasta_path.name} ")
-
+def read_file_Tnd(fasta_path):
+    '''
+    Returns list of time (numpy datetime64), no. of levels (int) and domains (int)
+    '''
     # 1. run ddhtoolbox
     # 1.1  determine number of  levels, domains and time-steps for file
     result = subprocess.run([ "ddhr", '-b', '-es', '-d', '-n',
@@ -55,45 +52,69 @@ def worker_ddh(fasta_path, articles = None):
     D = int(Dnd[0]) # Duration in s
     d = int(Dnd[1]) # no. of domains
     n = int(Dnd[2]) # no. of levels
-
     time = np.datetime64(date, 's') + D
-    #print(f'Time = {time}, d = {d}, n = {n}')
 
-    ds = xr.Dataset()
-    for article in articles:
+    return [time, n, d]
 
-        #  Extract each article, reads from stdout
-        result_article = subprocess.run([
-           "lfac",
-           str(fasta_path),
-           article
-        ],
-        capture_output=True,
-        text=True,
-        check=True)
+def extract_article(fasta_path, article, d):
+    '''
+    Parses DDH file for given article, returns numpy array of size (levels, domains)
 
-        # article data
-        data_read = np.fromstring(result_article.stdout, sep ='\n')
-        data_read = data_read.reshape(d,n).transpose()
+    arguments:
+    fasta_path: DDH file name
+    article: article name
+    d: no. of domains
+    '''
+    result_article = subprocess.run([
+       "lfac",
+       str(fasta_path),
+       article
+    ],
+    capture_output=True,
+    text=True,
+    check=True)
 
-        #  store data as DataArray for article
-        da = xr.DataArray(data = data_read,
-                dims = [ 'levels', 'domain' ],
-                coords={'levels':np.arange(n)+1,
-                    'domain' : np.arange(d)+1,
-                    'time': time
-                    },
-                name = article
-                )
+    data = np.fromstring(result_article.stdout, sep ='\n')
+    n = int(data.shape[0]/d)
+    return  data.reshape(d,n).transpose()
 
+def worker_ddh(chunk_list, worker_id, articles = None):
+    """
+    Worker: Runs Unix tools ddhr and lfac on DDH file list, extracts
+    articles, and returns a single xarray Dataset.
+    """
 
-        #  assemble article to DataSet
-        ds[article] = da
+    ds_list = []
+    time, n, d = read_file_Tnd(chunk_list[0])
 
-    ds.attrs['Number of Domains'] = d
-    ds.attrs['Number of levels'] = n
+    pbar = tqdm.tqdm(chunk_list, desc=f"Worker {worker_id}", position=worker_id + 1, leave=False)
 
-    return ds.expand_dims('time')
+    for file in pbar:
+        ds = xr.Dataset()
+
+        time, n, d = read_file_Tnd(file)
+        for article in articles:
+            data_read = extract_article(file, article, d)
+
+            #  store data as DataArray for article
+            da = xr.DataArray(data = data_read,
+                    dims = [ 'levels', 'domain' ],
+                    coords={'levels':np.arange(n)+1,
+                        'domain' : np.arange(d)+1,
+                        'time': time
+                        },
+                    name = article
+                    )
+
+            #  assemble article to DataSet
+            ds[article] = da
+
+        ds.attrs['Number of Domains'] = d
+        ds.attrs['Number of levels'] = n
+        ds_list.append(ds.expand_dims('time'))
+
+    # combine to dataset for this worker
+    return xr.combine_by_coords(ds_list)
 
 
 def dir_pipeline(dir_path, output_nc, articles):
@@ -113,28 +134,35 @@ def dir_pipeline(dir_path, output_nc, articles):
 
     ds_list = []
 
-    # 2.  Split file list among  threads for processing (parallel)
-    with ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
+    # 1. Determine number of workers
+    num_workers = os.cpu_count()
+    num_workers = 4
+    num_workers = num_workers or 1
+
+    # 2. Split files into chunks (OMP-style)
+    chunks = [list(c) for c in np.array_split(fasta_files, num_workers)]
+
+    # 3.  Split chunks among  threads for processing (parallel)
+    with ProcessPoolExecutor(max_workers=4) as executor:
         futures = [
-            executor.submit(worker_ddh,f, articles)
-            for f in fasta_files
+            executor.submit(worker_ddh, chunk,i, articles)
+            for i, chunk in enumerate(chunks)
         ]
-        for future in tqdm.tqdm(as_completed(futures), total=len(futures), desc="Processing Files"):
+        for future in tqdm.tqdm(as_completed(futures), total=len(futures), desc="Processing chunks"):
             result = future.result()
             ds_list.append(result)
+
 
     # 3. Assemble final DataSet
     full_ds = xr.combine_by_coords(ds_list)
     return full_ds
-
 
     # 4. Export
     full_ds.to_netcdf(output_nc)
     print(f"NetCDF saved to {output_nc}")
 
 
-
-with open('list') as file:
+with open('listFull2') as file:
     articles = [line.rstrip() for line in file]
 
 DS = dir_pipeline(dir_path = 'data',articles=articles, output_nc = 'out.nc')

6be24c1922187147f0dcefdfbe7e9ef81953cfaf
Abhi-smhi <abhilash.murlidharanmenon@smhi.se>
2026-04-25 21:09:46 +0000

    Completed reading DDH articles, lazy loading to xarray and writing to netcdf

--- ddh2nc.py
+++ ddh2nc.py
@@ -4,7 +4,7 @@ import numpy as np
 import glob as glob
 import dask.array as da
 import dask
-from dask.distributed import Client, get_client
+from dask.distributed import Client, get_client, progress
 
 # ┌────────────────────────────────────────────────────────────────────────────┐
 # │                           USER CONFIG STARTS HERE                          │
@@ -13,18 +13,22 @@ INPUT_DIR       = "/ec/res4/scratch/swe7088/deode/osm_pgd_ddh_CY49t2_HARMONIE_AR
 PATTERN         = 'DHFDLDEOD*s'
 OUTPUT_FILE     = "/perm/swe7088/dask_test_out.nc"
 ARTICLE_LIST    = "ddh_article_list2"
-NWORKER         = 1
-MEMLIMIT        = '2GB'
+NWORKER         = 2
+MEMLIMIT        = '4GB'
 # ┌────────────────────────────────────────────────────────────────────────────┐
 # │                           USER CONFIG ENDS HERE                            │
 # └────────────────────────────────────────────────────────────────────────────┘
 
 def read_file_T(fasta_path):
     '''
-    Returns list of time (numpy datetime64), no. of levels (int) and domains (int)
+    Extracts validity of DDH file
+
+    param:
+    fasta_path: (str) DDH file path
+
+    return:
+    time: (np.datetime64) Validity
     '''
-    # 1. run ddhtoolbox
-    # 1.1  determine number of  levels, domains and time-steps for file
 
     result = subprocess.run([ "ddhr", '-b', '-es',
         str(fasta_path)
@@ -42,11 +46,16 @@ def read_file_T(fasta_path):
 
 def read_file_nd(fasta_path):
     '''
-    Returns list of time (numpy datetime64), no. of levels (int) and domains (int)
-    '''
-    # 1. run ddhtoolbox
-    # 1.1  determine number of  levels, domains and time-steps for file
+    Extracts no. of domains and levels of DDH file
 
+    param:
+    fasta_path: (str) DDH file path
+
+    return:
+    n: (int) no. of levels
+    d: (int) no. of domains
+
+    '''
     result = subprocess.run([ "ddhr", '-d', '-n',
         str(fasta_path)
         ],
@@ -61,11 +70,34 @@ def read_file_nd(fasta_path):
     return n, d
 
 
+def read_DDH_data(path, articles, n_levels, n_domains):
+
+    data = np.zeros([len(articles), n_levels, n_domains])
+    for i, article in enumerate(articles):
+        result_article = subprocess.run([
+           "lfac",
+           str(path),
+           article
+        ],
+        capture_output=True,
+        text=True,
+        check=True)
+
+        data[i, :,:] = (np.fromstring(result_article.stdout, sep ='\n')
+                .reshape(n_domains,n_levels).transpose())
+
+
+    return data
+
+
+
+
 if __name__ == "__main__":
     xr.set_options(use_new_combine_kwarg_defaults=True) # To set coordinates explicitly
 
     with open(ARTICLE_LIST) as file:
         articles = [line.rstrip() for line in file]
+
 
     # 1. Setup Dask Cluster
     try:
@@ -73,28 +105,69 @@ if __name__ == "__main__":
         print(f"Using existing client: {client}")
     except ValueError:
         client = Client(n_workers=NWORKER, threads_per_worker=1, memory_limit=MEMLIMIT)
+
+      # client = Client()
         print(f"Created new dask client: {client}")
 
-    file_list = sorted(glob.glob(INPUT_DIR + PATTERN))[0:1000]
+    file_list = sorted(glob.glob(INPUT_DIR + PATTERN))[0:500]
 
     n_times = len(file_list)
     print(f'Found {n_times} DDH files')
 
-    # -- Parse no. of files and domains
+    # 2. Parse no. of files and domains
 
     n_levels, n_domains = read_file_nd(file_list[0])
     print(f'DDH files contain {n_domains} domains, of {n_levels} levels')
 
     # -- prepare dask delayed functions
     read_time_delayed = dask.delayed(read_file_T)
+    read_DDH_delayed  = dask.delayed(read_DDH_data)
+
+    # 3. Assemble dask arrays
 
     # -- assess times for all files
-    times = [read_time_delayed(file) for file in file_list]
-    time_d = [da.from_delayed(time, shape=(), dtype='datetime64') for time in times]
-    time_stack = da.stack(time_d, axis=0)
+    print(f'[Lazy] Assebling dask array for validites')
+    times_list = [read_time_delayed(file) for file in file_list]
+    # ---- dask array
+    time_da = [da.from_delayed(time, shape=(), dtype='datetime64')
+            for time in times_list]
+    time_stack = da.stack(time_da, axis=0)
+
+    print(f'[Lazy] Assebling dask array for DDH data')
+    data_list = [read_DDH_delayed(path=file, articles=articles,
+        n_domains=n_domains, n_levels=n_levels)
+            for file in file_list]
+    # ---- dask array
+    data_da = [da.from_delayed(data,
+        shape=(len(articles), n_levels, n_domains),
+        dtype='float64')
+        for data in data_list]
+    da_stack = da.stack(data_da, axis=0)
 
 
+    # --  Assemble the xarray (dask) dataset
+    data_vars = {}
+    print(f'[Lazy] Assembling xarray dataset')
+    for i, article in enumerate(articles):
+        data_vars[article] = (['time', 'level', 'domain'], da_stack[:, i,:,:])
 
+    ds_da = xr.Dataset(
+            data_vars=data_vars,
+            coords = {
+                'time': time_stack,
+                'level': np.arange(n_levels) +1,
+                'domain': np.arange(n_domains) +1,
+                }
+            )
+    print(f'[Lazy] Chunking dataset')
+    ds_da = ds_da.chunk({'time':'auto','level':-1, 'domain':-1})
+    print(f'[Lazy] Preparing to write')
+    jobs = ds_da.to_netcdf(OUTPUT_FILE, mode='w', engine='h5netcdf', compute=False)
 
+    print(f'Compute and writing to {OUTPUT_FILE}')
+    futures = client.compute(jobs)
+    progress(futures)
 
+    print(f'Job done!\nClosing dask client')
+    client.close()
 

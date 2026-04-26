f53dd4dd920f5450af78edcd0bee4bc93524ff05
Abhi-smhi <abhilash.murlidharanmenon@smhi.se>
2026-04-26 17:38:28 +0000

    Swtiching to batch wise reading to reduce task graph. Only time data is read 'eagerly', DDH data read Lazily

--- ddh2nc.py
+++ ddh2nc.py
@@ -3,6 +3,7 @@ import xarray as xr
 import numpy as np
 import glob as glob
 import dask.array as da
+import dask.bag as db
 import dask
 from dask.distributed import Client, get_client, progress
 
@@ -13,7 +14,8 @@ INPUT_DIR       = "/ec/res4/scratch/swe7088/deode/osm_pgd_ddh_CY49t2_HARMONIE_AR
 PATTERN         = 'DHFDLDEOD*s'
 OUTPUT_FILE     = "/perm/swe7088/dask_test_out.zarr"
 ARTICLE_LIST    = "ddh_article_list2"
-NWORKER         = 16
+NWORKER         = 4
+BATCH_SIZE      = 100
 MEMLIMIT        = '16GB'
 # ┌────────────────────────────────────────────────────────────────────────────┐
 # │                           USER CONFIG ENDS HERE                            │
@@ -30,19 +32,22 @@ def read_file_T(fasta_path):
     time: (np.datetime64) Validity
     '''
 
-    result = subprocess.run([ "ddhr", '-b', '-es',
-        str(fasta_path)
-        ],
-        capture_output=True,
-        text=True,
-        check=True)
+    try:
+        result = subprocess.run([ "ddhr", '-b', '-es',
+            str(fasta_path)
+            ],
+            capture_output=True,
+            text=True,
+            check=True)
 
-    result = result.stdout
-    start = result[0:10] + 'T' + result[11:16]
-    D = np.fromstring(result[17:], sep='\n')[0] # Duration in s
-    time = np.datetime64(start, 's') + int(D)
+        result = result.stdout
+        start = result[0:10] + 'T' + result[11:16]
+        D = np.fromstring(result[17:], sep='\n')[0] # Duration in s
+        time = np.datetime64(start, 's') + int(D)
 
-    return time
+        return time
+    except:
+        return np.datetime64('NaT')
 
 def read_file_nd(fasta_path):
     '''
@@ -56,6 +61,7 @@ def read_file_nd(fasta_path):
     d: (int) no. of domains
 
     '''
+
     result = subprocess.run([ "ddhr", '-d', '-n',
         str(fasta_path)
         ],
@@ -69,9 +75,7 @@ def read_file_nd(fasta_path):
 
     return n, d
 
-
 def read_DDH_data(path, articles, n_levels, n_domains):
-
     data = np.zeros([len(articles), n_levels, n_domains])
     for i, article in enumerate(articles):
         result_article = subprocess.run([
@@ -85,11 +89,16 @@ def read_DDH_data(path, articles, n_levels, n_domains):
 
         data[i, :,:] = (np.fromstring(result_article.stdout, sep ='\n')
                 .reshape(n_domains,n_levels).transpose())
-
-
     return data
 
 
+def read_batch(files, articles, n_levels, n_domains):
+    data_batch = []
+    for f in files:
+        data = read_DDH_data(f, articles, n_levels, n_domains)
+        data_batch.append(data)
+
+    return np.stack(data_batch, axis=0)
 
 
 if __name__ == "__main__":
@@ -98,71 +107,50 @@ if __name__ == "__main__":
     with open(ARTICLE_LIST) as file:
         articles = [line.rstrip() for line in file]
 
-    file_list = sorted(glob.glob(INPUT_DIR + PATTERN))
+    file_list = sorted(glob.glob(INPUT_DIR + PATTERN))[0:150]
 
     n_times = len(file_list)
+    print(f'\n\n-------------  INFO  -------------')
     print(f'Found {n_times} DDH files')
 
     # 2. Parse no. of files and domains
 
     n_levels, n_domains = read_file_nd(file_list[0])
     print(f'DDH files contain {n_domains} domains, of {n_levels} levels')
+    print(f'\----------------------------------')
 
 
     with Client(n_workers=NWORKER, threads_per_worker=1, memory_limit=MEMLIMIT) as client:
+        print(f'\n\nDask client: {client}\n\n')
 
-        print(f'\n\nDask client: {client}')
+        files_bag = db.from_sequence(file_list, npartitions=NWORKER*4)
 
-        # -- prepare dask delayed functions
-        read_time_delayed = dask.delayed(read_file_T)
-        read_DDH_delayed  = dask.delayed(read_DDH_data)
+        print('[Dask] Computing DDH validities')
+        processed_time = files_bag.map(read_file_T)
+        persist_times = client.persist(processed_time)
+        progress(persist_times)
+        actual_times = np.array(persist_times.compute())
 
-        # 3. Assemble dask arrays
+        print('Reading DDH data')
 
-        # -- assess times for all files
-        print(f'[Lazy] Assebling dask array for validites')
-        times_list = [read_time_delayed(file) for file in file_list]
-        # ---- dask array
-        time_da = [da.from_delayed(time, shape=(), dtype='datetime64')
-                for time in times_list]
-        time_stack = da.stack(time_da, axis=0)
+        # ---  saves the scheduler from pickling the entire list for every woker
+        articles_future = client.scatter(articles, broadcast=True)
+        file_batches = [file_list[i:i+BATCH_SIZE] for i in range(0, len(file_list), BATCH_SIZE)]
 
-        print(f'[Lazy] Assebling dask array for DDH data')
-        data_list = [read_DDH_delayed(path=file, articles=articles,
-            n_domains=n_domains, n_levels=n_levels)
-                for file in file_list]
-        # ---- dask array
-        data_da = [da.from_delayed(data,
-            shape=(len(articles), n_levels, n_domains),
-            dtype='float64')
-            for data in data_list]
-        da_stack = da.stack(data_da, axis=0)
+        lazy_batches = []
 
+        for batch in file_batches:
+            current_shape = (len(batch), len(articles), n_levels, n_domains)
 
-        # --  Assemble the xarray (dask) dataset
-        print(f'[Lazy] Convert to Dataarray')
-        da_lazy = xr.DataArray(
-                data=da_stack,
-                dims=['time','article', 'level', 'domain'],
-                coords = {
-                    'time': time_stack,
-                    'article': articles,
-                    'level': np.arange(n_levels) +1,
-                    'domain': np.arange(n_domains) +1,
-                    },
-                )
-        print(f'[Lazy] Chunking dataset')
-        ds_da = da_lazy.to_dataset(dim='article')
-        ds_da = ds_da.chunk({'time':'auto','level':-1, 'domain':-1})
+            d_part  = dask.delayed(read_batch)(batch, articles_future, n_levels, n_domains)
 
-        print(f'[Lazy] Assembled Dataset .. ')
-        print(ds_da)
+            b_array = da.from_delayed(d_part, shape=current_shape, dtype='float64')
 
-        print(f'[Lazy] Preparing to write')
-        jobs = ds_da.to_zarr(OUTPUT_FILE, mode='w', compute=False)
-        print(f'Compute and writing to {OUTPUT_FILE}')
-        futures = client.compute(jobs)
-        progress(futures)
+            lazy_batches.append(b_array)
 
-        print(f'Job done!\nClosing dask client')
+        da_stack = da.concatenate(lazy_batches, axis=0)
+        print(f'[Dask] Reading data in {len(file_batches)} batches')
+        persist_data = client.persist(da_stack)
+        progress(persist_data)
+
 
